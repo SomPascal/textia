de4a40e6efe7f37b933cab952053b8f728ed7eec
SomPascal <somfuncky8@gmail.com>
2025-07-12 19:50:29 +0100

    fixed null error

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -8,9 +8,10 @@ export default function TextInput({
   return (
     <div className='p-3 rounded-md bg-base-200 w-12/12 md:w-10/12 lg:w-6/12 mb-4 md:mb-7'>
         <textarea
-            onKeyUp={(e)=> { setValue(e?.target.value ?? '') }}
+            onKeyUp={(e)=> { setValue(e.target?.value ?? '') }}
             className='w-full border-0 outline-0 resize-y text-lg min-h-25' 
             placeholder='Your text here...'
+            value={value}
         >{value}</textarea>
     </div>
   )
